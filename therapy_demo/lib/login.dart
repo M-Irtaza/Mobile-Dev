22813c1d96649b7c1bcc275c48afcec75131d3f5
Syed Muhammad Irtaza Haider <irtaza124000@gmail.com>
2022-04-16 23:52:16 +0500

    Login-screen
    Update 2

--- therapy_demo/lib/login.dart
+++ therapy_demo/lib/login.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
+  static const route = '/login';
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
@@ -8,10 +9,40 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool _isHidden = true;
+  bool _rememberMe = false;
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _togglePasswordView() {
+    setState(() {
+      _isHidden = !_isHidden;
+    });
+  }
+
+  String? _validateEmail(value) {
+    if (value!.isEmpty) return 'Enter Password';
+    final regex =
+        RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$');
+    if (!regex.hasMatch(value)) {
+      return 'Password must be at least 8 charactes long containing at least one uppercase letter, one number and one special character';
+    }
+    return null;
+  }
+
+  void _validate() {
+    final form = _formKey.currentState;
+    if (!form!.validate()) {
+      return;
+    }
+    //Navigator.of(context)
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
+      resizeToAvoidBottomInset: true,
       appBar: AppBar(
         backgroundColor: Colors.yellow,
         title: Text(
@@ -54,22 +85,27 @@ class _MyHomePageState extends State<MyHomePage> {
                       labelText: 'Password',
                     ),
                   ),
-                  Container(
-                    color: Colors.white,
-                    height: 30,
+                  Row(
+                    children: [
+                      Row(
+                          //children: [Checkbox(value: true, onChanged: () {})],
+                          ),
+                    ],
                   ),
                   SizedBox(
                     width: 250,
                     child: ElevatedButton(
                       style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.yellow),
                           shape:
                               MaterialStateProperty.all<RoundedRectangleBorder>(
                                   RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ))),
+                            borderRadius: BorderRadius.circular(20),
+                          ))),
                       child: const Text(
                         'Login',
-                        style: TextStyle(fontSize: 18),
+                        style: TextStyle(fontSize: 18, color: Colors.black),
                       ),
                       onPressed: () {},
                     ),
